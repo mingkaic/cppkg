14a8035374680fe3e5aea7db6146d2d9119860fb
Ming Kai Chen <6446445+mingkaic@users.noreply.github.com>
2019-09-02 20:37:24 -0700

    Allow scope guard to be inheritable (#8)
    * allow scope guard inheritable
    
    * add prefix affix checks

--- estd/test/main.cpp
+++ estd/test/main.cpp
@@ -8,6 +8,7 @@
 
 #include "estd/estd.hpp"
 #include "estd/range.hpp"
+#include "estd/strs.hpp"
 
 
 int main (int argc, char** argv)
@@ -229,6 +230,32 @@ TEST(RANGE, Between)
 		EXPECT_TRUE(range.between(i));
 	}
 	EXPECT_FALSE(range.between(55));
+}
+
+
+TEST(STRS, HasPrefix)
+{
+	EXPECT_FALSE(estd::has_prefix("abcdef", "def"));
+	EXPECT_FALSE(estd::has_prefix("abcdef", "ghidef"));
+	EXPECT_FALSE(estd::has_prefix("abcdef", "ghijkl"));
+	EXPECT_FALSE(estd::has_prefix("abcdef", "f"));
+	EXPECT_FALSE(estd::has_prefix("abcdef", "abchij"));
+	EXPECT_TRUE(estd::has_prefix("abcdef", "abcdef"));
+	EXPECT_TRUE(estd::has_prefix("abcdef", "abc"));
+	EXPECT_TRUE(estd::has_prefix("abcdef", "a"));
+}
+
+
+TEST(STRS, HasAffix)
+{
+	EXPECT_FALSE(estd::has_affix("abcdef", "abc"));
+	EXPECT_FALSE(estd::has_affix("abcdef", "abcjkl"));
+	EXPECT_FALSE(estd::has_affix("abcdef", "ghijkl"));
+	EXPECT_FALSE(estd::has_affix("abcdef", "a"));
+	EXPECT_FALSE(estd::has_affix("abcdef", "ghidef"));
+	EXPECT_TRUE(estd::has_affix("abcdef", "abcdef"));
+	EXPECT_TRUE(estd::has_affix("abcdef", "def"));
+	EXPECT_TRUE(estd::has_affix("abcdef", "f"));
 }
 
 
