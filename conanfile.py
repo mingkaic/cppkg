854beca079d8f74749911a5f2f854a5dc6546532
Ming Kai Chen <mingkaichen2009@gmail.com>
2020-11-23 01:00:07 -0500

    add cmake tests

--- conanfile.py
+++ conanfile.py
@@ -4,10 +4,14 @@ import os
 
 class CppkgConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "boost/1.73.0", "grpc/1.29.1@inexorgame/stable"
+    requires = "boost/1.73.0", "grpc/1.29.1@inexorgame/stable", "gtest/1.10.0"
     generators = "cmake", "cmake_find_package_multi"
 
-    def build(self):
+    def _configure_cmake(self):
         cmake = CMake(self)
         cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
         cmake.build()
